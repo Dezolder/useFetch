522d3779230ea788c32c86272c43e9ff46950868
Dezolder <evgeniy.zhukov.2012@gmail.com>
2023-03-12 12:39:02 +0200

    Fix2

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,30 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [params, setParams] = useState({});
 
   const refetch = (params) => {
-    setParams(params)
+    fetchData(params);
   };
 
-  useEffect(() => {
-    const fetchData = async (params) => {
-      setLoading(true);
-      try {
-        const response = await axios.get(url, params);
-        setData(response.data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData(params);
-  }, [url, params]);
+  const fetchData = async (params) => {
+    setLoading(true);
+    try {
+      const response = await axios.get(url, params);
+      setData(response.data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return { data, isLoading, error, refetch };
 };
