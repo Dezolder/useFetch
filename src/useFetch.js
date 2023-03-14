2aaec4328b792f770ae0d44636a3f6e38b42db7b
Dezolder <evgeniy.zhukov.2012@gmail.com>
2023-03-14 02:39:09 +0200

    Fix3

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,26 +1,29 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 
-export const useFetch = (url) => {
-  const [data, setData] = useState(null);
+export const useFetch = (url, params = {}) => {
+  const [data, setData] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const refetch = (params) => {
-    fetchData(params);
-  };
-
-  const fetchData = async (params) => {
+  const fetchData = useCallback(async (param = params) => {
+    console.log('fetchData')
     setLoading(true);
     try {
-      const response = await axios.get(url, params);
+      const response = await axios.get(url, param);
       setData(response.data);
     } catch (err) {
       setError(err.message);
     } finally {
       setLoading(false);
     }
-  };
+  }, [params, url] // Ошибка если пустой массив!
+  )
 
-  return { data, isLoading, error, refetch };
+  useEffect(() => {
+    console.log('useEffect')
+    async function fetchData(params) { }
+    fetchData() // так useEffect обрабатывает промисы!
+  }, [])
+  return { data, isLoading, error, refetch: fetchData };
 };
